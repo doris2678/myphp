b7cd113ef151233bca5a11cd623392109ec4bfe7
doris <dorishsh2678@gmail.com>
2025-04-28 11:33:40 +0800

    add select

--- select/index.php
+++ select/index.php
@@ -17,7 +17,7 @@
 
 <?php
 //給定一個成績數字，判斷是否及格(60)分
- $score="aa";
+ $score="50";
  if(!is_numeric($score) || $score<0 || $score >100){
    echo "請輸入合法的成績數字";
    exit();
@@ -32,27 +32,78 @@
   }else {
     echo "<span style='color:red'>不及格</span>";
   }
+?>
+
+ <h2>分配成績等級</h2>
+ <p>給定一個成績數字，根據成績所在的區間,給定等級</p>
+ <ul>
+    <li>0 ~ 59 => E</li>
+    <li>60 ~ 69 => D</li>
+    <li>70 ~ 79 => C</li>
+    <li>80 ~ 89 => B</li>
+    <li>90 ~ 100 => A</li>
+ </ul>
+<?php
+ $level='';
+if ($score<60) {
+    $level='E';    
+}elseif($score>=60 && $score<70){
+    $level='D';    
+}elseif($score>=70 && $score<80){
+    $level='C';    
+}elseif($score>=80 && $score<90){
+    $level='B';    
+}
+else {
+    $level='A';    
+}
+
+echo "<br>";
+echo $level;
+?>
+<h2>依據學生成績等級給予評價</h2>
+<p>根據學生不同的成績等級在網頁上印出不同的文字評價</p>
+<?php
+switch ($level) {
+    case 'A':
+        echo "good1";
+    break;
+    case 'B':
+        echo "good2";
+    break;
+    case 'C':
+        echo "good3";
+    break;
+    case 'D':
+        echo "good4";
+    break;
+    case 'E':
+        echo "good5";
+    break;            
+    default:    
+        echo "error";
+    
+}
+
+?>
+<h2>閏年判斷，給定一個西元年份，判斷是否為閏年</h2>
+<ul>
+  <li>地球對太陽的公轉一年的真實時間大約是365天5小時48分46秒，因此以365天定為一年 的狀況下，每年會多出近六小時的時間，所以每隔四年設置一個閏年來消除這多出來的一天。</li>
+  <li>公元年分除以4不可整除，為平年。</li>
+  <li>公元年分除以4可整除但除以100不可整除，為閏年。</li>
+  <li>公元年分除以100可整除但除以400不可整除，為平年。</li>
+</ul>
+<?php
+$year=2300;
+
+if (($year%4==0 && $year % 100 != 0) || ($year % 400==0)) {
+  echo "閏年";    
+}else {        
+ echo "平年";            
+}         
 
 
-//0 ~ 59 => E
-//60 ~ 69 => D
-//70 ~ 79 => C
-//80 ~ 89 => B
-//90 ~ 100 => A
-// $score2=90;
-// if ($score2<60) {
-//     echo "E";    
-// }elseif($score2>=60 && $score2<70){
-//     echo "D";    
-// }elseif($score2>=70 && $score2<80){
-//     echo "C";    
-// }elseif($score2>=80 && $score2<90){
-//     echo "B";    
-// }
-// else {
-//     echo "A";    
-// }
-
+  
 ?>
 </body>
 </html>
