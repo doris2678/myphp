c0ff66f4d8d62a5751ff2195d0ffb8b6837cd79b
doris <dorishsh2678@gmail.com>
2025-05-02 08:57:42 +0800

    20250502 加首頁

--- index.php
+++ index.php
@@ -1,26 +1,37 @@
-<?php
-define("PI",3.1415);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>程式基礎</title>
+    <style>
+      ul{
+        list-style-type:none;        
+        display:flex;
+        justify-content:center;
+        flex-wrap:wrap;
+        width: 70%$;        
+        paddding:0 5%;       
+      }
+      li{
+        paddding: 10px 20 px;
+        border:1px solid blue;
+        border-radius:15px;
+        box-shadow:1 px 1px 5px #eee; 
+        margin:1%;
+      }
 
-$age=54;
-$name="黃秀慧";
-$ename="doris";
 
-echo '我的年齡是：'.$age.'歲';
-echo "<br>";
-echo '我的姓名：'.$name; 
-echo "<br>";
-echo '我的英文名字：'.$ename; 
-echo "<br>";
-echo PI;
-echo "<hr>";
 
-$age=20;
-$name="王小明";
-echo "我的年齡是：".$age."歲";
-echo "<br>";
-echo '我的姓名：'.$name; 
-echo "<br>";
-echo '我的英文名字：'.$ename; 
-echo "<br>";
-echo "<hr>";
-echo PI;
+    </style>
+</head>
+<body>
+    <ul>
+        <li><a href="var.php">變數</a></li>
+        <li><a href="./select/index.php">判斷式</a></li>
+        <li><a href="./loop/index.php">重覆結構</a></li>
+        <li><a href="./array/index.php">陣列</a></li>
+        <li><a href="./array/index.php">字串</a></li>
+    </ul>
+</body>
+</html>
---
+++ var.php
@@ -0,0 +1,26 @@
+<?php
+define("PI",3.1415);
+
+$age=54;
+$name="黃秀慧";
+$ename="doris";
+
+echo '我的年齡是：'.$age.'歲';
+echo "<br>";
+echo '我的姓名：'.$name; 
+echo "<br>";
+echo '我的英文名字：'.$ename; 
+echo "<br>";
+echo PI;
+echo "<hr>";
+
+$age=20;
+$name="王小明";
+echo "我的年齡是：".$age."歲";
+echo "<br>";
+echo '我的姓名：'.$name; 
+echo "<br>";
+echo '我的英文名字：'.$ename; 
+echo "<br>";
+echo "<hr>";
+echo PI;
